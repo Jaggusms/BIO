739aa7e04c4cef2f6ce3e8246f103cc7c67a5890
Jaggusms <38516306+Jaggusms@users.noreply.github.com>
2022-12-04 23:36:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import pandas as pd
-print("hey kousar, how are you!. take care")
+from flask import Flask
+ 
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+ 
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+ 
+# main driver function
+if __name__ == '__main__':
+    print("hey kousar, how are you!. take care")
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
+
